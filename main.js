4489ce12e88260ea53bad7c596dfc94f47f62489
Kurrtod <kurrxdstore@gmail.com>
2021-12-27 03:23:34 +0700

    web-bot-nulis

---
+++ main.js
@@ -0,0 +1,11 @@
+__path = process.cwd()
+
+var express = require('express');
+var router = express.Router();
+
+router.get('/', (req, res) => {
+    res.sendFile(__path + '/index.html')
+
+})
+
+module.exports = router
